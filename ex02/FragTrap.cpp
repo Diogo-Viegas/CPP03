0424e683f3524de17b79315ee7f60ccbf75b73d9
Diogo Viegas <dviegas@c2r3s10.42lisboa.com>
2026-06-08 16:08:49 +0100

    more fixes

--- ex02/FragTrap.cpp
+++ ex02/FragTrap.cpp
@@ -11,7 +11,7 @@ FragTrap::FragTrap() : ClapTrap()
 
 FragTrap::FragTrap(const std::string name) : ClapTrap(name)
 {
-    std::cout << BLUE << "FragTrap -> Name Constructor Called" << RESET << std::endl;
+    std::cout << BLUE << "FragTrap -> constructed with name '" << name << "'." << RESET << std::endl;
     _hitPoints = 100;
     _energyPoints = 100;
     _attackDamage = 30;
@@ -38,20 +38,20 @@ FragTrap& FragTrap::operator=(const FragTrap &obj)
 void FragTrap::attack(const std::string& target)
 {
     if(_energyPoints <= 0 || _hitPoints <= 0)
-        std::cout << BLUE << "FragTrap " << _name << ": has insufficient hit points or energy to attack." << RESET << std::endl;
+        std::cout << BLUE << "FragTrap " << _name << " has insufficient hit points or energy to attack." << RESET << std::endl;
     else
     {
-        std::cout << BLUE << "FragTrap " << _name << ": attacks " << target << ", dealing " << _attackDamage << " damage." << RESET << std::endl;
+        std::cout << BLUE << "FragTrap " << _name << " attacks " << target << ", dealing " << _attackDamage << " damage." << RESET << std::endl;
         _energyPoints--;
     }
 }
 void FragTrap::takeDamage(unsigned int amount)
 {
     if(_hitPoints <= 0)
-        std::cout << BLUE << "FragTrap " << _name << ": cannot take more damage (hit points are already 0)." << RESET << std::endl;
+        std::cout << BLUE << "FragTrap " << _name << " cannot take more damage (hit points are already 0)." << RESET << std::endl;
     else
     {
-        std::cout << BLUE << "FragTrap " << _name << ": took " << amount << " damage." << RESET << std::endl;
+        std::cout << BLUE << "FragTrap " << _name << " took " << amount << " damage." << RESET << std::endl;
         _hitPoints = _hitPoints - amount;
     }
     
@@ -60,10 +60,10 @@ void FragTrap::takeDamage(unsigned int amount)
 void FragTrap::beRepaired(unsigned int amount)
 {
     if(_energyPoints <= 0 || _hitPoints <= 0)
-        std::cout << BLUE << "FragTrap " << _name << ": has insufficient hit points or energy to repair." << RESET << std::endl;
+        std::cout << BLUE << "FragTrap " << _name << " has insufficient hit points or energy to repair." << RESET << std::endl;
     else
     {
-        std::cout << BLUE << "FragTrap " << _name << ": repaired " << amount << " hit points." << RESET << std::endl;
+        std::cout << BLUE << "FragTrap " << _name << " repaired " << amount << " hit points." << RESET << std::endl;
         setHitPoints(_hitPoints + amount);    
         _energyPoints--;
     }   
--- ex02/ScavTrap.cpp
+++ ex02/ScavTrap.cpp
@@ -2,7 +2,7 @@
 
 ScavTrap::ScavTrap():ClapTrap()
 {
-    std::cout << RED << "ScavTrap: default constructed." << RESET << std::endl;
+    std::cout << RED << "ScavTrap -> default constructed." << RESET << std::endl;
     _name = "No name";
     _hitPoints = 100;
     _energyPoints = 50;
@@ -10,10 +10,9 @@ ScavTrap::ScavTrap():ClapTrap()
     _openGate = false;
 }
 
-ScavTrap::ScavTrap(std::string name):ClapTrap()
+ScavTrap::ScavTrap(std::string name):ClapTrap(name)
 {
-    std::cout << RED << "ScavTrap: constructed with name '" << name << "'." << RESET << std::endl;
-    _name = name;
+    std::cout << RED << "ScavTrap -> constructed with name '" << name << "'." << RESET << std::endl;
     _hitPoints = 100;
     _energyPoints = 50;
     _attackDamage = 20;
@@ -21,12 +20,12 @@ ScavTrap::ScavTrap(std::string name):ClapTrap()
 }
 ScavTrap::ScavTrap(const ScavTrap &obj):ClapTrap()
 {
-    std::cout << RED<< "ScavTrap: copy constructed from '" << obj._name << "'." << RESET<< std::endl;
+    std::cout << RED<< "ScavTrap -> copy constructed from '" << obj._name << "'." << RESET<< std::endl;
     *this = obj;
 }
 ScavTrap& ScavTrap::operator=(const ScavTrap &obj)
 {
-    std::cout << RED << "ScavTrap: assigned from '" << obj._name << "'." << RESET << std::endl;
+    std::cout << RED << "ScavTrap -> assigned from '" << obj._name << "'." << RESET << std::endl;
     if(this != &obj)
     {
         _name = obj._name;
@@ -41,20 +40,20 @@ ScavTrap& ScavTrap::operator=(const ScavTrap &obj)
 void ScavTrap::attack(const std::string& target)
 {
     if(_energyPoints <= 0 || _hitPoints <= 0)
-        std::cout << RED << "ScavTrap " << _name << ": has insufficient hit points or energy to attack." << RESET << std::endl;
+        std::cout << RED << "ScavTrap " << _name << " has insufficient hit points or energy to attack." << RESET << std::endl;
     else
     {
-        std::cout << RED << "ScavTrap " << _name << ": attacks " << target << ", dealing " << _attackDamage << " damage." << RESET << std::endl;
+        std::cout << RED << "ScavTrap " << _name << " attacks " << target << ", dealing " << _attackDamage << " damage." << RESET << std::endl;
         _energyPoints--;
     }
 }
 void ScavTrap::takeDamage(unsigned int amount)
 {
     if(_hitPoints <= 0)
-        std::cout << RED << "ScavTrap " << _name << ": cannot take more damage (hit points are already 0)." << RESET << std::endl;
+        std::cout << RED << "ScavTrap " << _name << " cannot take more damage (hit points are already 0)." << RESET << std::endl;
     else
     {
-        std::cout << RED << "ScavTrap " << _name << ": took " << amount << " damage." << RESET << std::endl;
+        std::cout << RED << "ScavTrap " << _name << " took " << amount << " damage." << RESET << std::endl;
         _hitPoints = _hitPoints - amount;
     }
     
@@ -63,10 +62,10 @@ void ScavTrap::takeDamage(unsigned int amount)
 void ScavTrap::beRepaired(unsigned int amount)
 {
     if(_energyPoints <= 0 || _hitPoints <= 0)
-        std::cout << RED << "ScavTrap " << _name << ": has insufficient hit points or energy to repair." << RESET << std::endl;
+        std::cout << RED << "ScavTrap " << _name << " has insufficient hit points or energy to repair." << RESET << std::endl;
     else
     {
-        std::cout << RED << "ScavTrap " << _name << ": repaired " << amount << " hit points." << RESET << std::endl;
+        std::cout << RED << "ScavTrap " << _name << " repaired " << amount << " hit points." << RESET << std::endl;
         setHitPoints(_hitPoints + amount);    
         _energyPoints--;
     }   
@@ -77,13 +76,13 @@ void	ScavTrap::guardGate(void)
 	if (this->_openGate == false)
 	{
 		_openGate = true;
-		std::cout << RED << "ScavTrap " << _name << ": is now guarding the gate." << RESET << std::endl;
+		std::cout << RED << "ScavTrap " << _name << " is now guarding the gate." << RESET << std::endl;
 	}
 	else
-		std::cout << RED << "ScavTrap " << _name << ": is already guarding the gate." << RESET << std::endl;
+		std::cout << RED << "ScavTrap " << _name << " is already guarding the gate." << RESET << std::endl;
 }
 
 ScavTrap::~ScavTrap()
 {
-    std::cout << RED << "ScavTrap "<< _name << ": Destructor called"<< RESET << std::endl;
+    std::cout << RED << "ScavTrap "<< _name << "-> Destructor called"<< RESET << std::endl;
 }
